915f5cb144a5467a82d2b64f98a19afe18ba8648
Alex Gaynor <alex.gaynor@gmail.com>
2007-10-08 16:56:58 +0000

    Fixed the URLconf
    git-svn-id: file:///Users/brian/Backups/S3/backups/Wednesday/brian/svn/djog/trunk@25 95ab7de8-677b-4562-a266-b8acc9dd993c

--- djog/urls.py
+++ djog/urls.py
@@ -24,11 +24,11 @@ feeds = {
 #
 urlpatterns = patterns('django.views.generic.date_based',
 	(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[-\w]+)/$',
-        'object_detail', dict(blog_dict, slug_field='slug', name='djog_entry')),
-    (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', 'archive_day', dict(date_dict, name='djog_daily')),
-    (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', 'archive_month', dict(date_dict, name='djog_monthly')),
-    (r'^(?P<year>\d{4})/$', 'archive_year', dict(date_dict, name='djog_yearly')),
-    (r'^$', 'archive_index', dict(blog_dict, name='djog_index'),
+        'object_detail', dict(blog_dict, slug_field='slug'), name='djog_entry'),
+    (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', 'archive_day', date_dict, name='djog_daily'),
+    (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', 'archive_month', date_dict, name='djog_monthly'),
+    (r'^(?P<year>\d{4})/$', 'archive_year', date_dict, name='djog_yearly'),
+    (r'^$', 'archive_index', blog_dict, name='djog_index'),
 )
 
 urlpatterns += patterns('',
