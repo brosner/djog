763761214305b5f20cf9d8f1d802636293f81577
Brian Rosner <brosner@gmail.com>
2007-10-08 17:50:31 +0000

    Added some code for setup.py.
    git-svn-id: file:///Users/brian/Backups/S3/backups/Wednesday/brian/svn/djog/trunk@30 95ab7de8-677b-4562-a266-b8acc9dd993c

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
-# will soon be the setuptools script to install djog
+
+from setuptools import setup
+
+version_bits = __import__('djog').VERSION
+if version_bits[2] is not None:
+    version = '%d.%d_%s' % version_bits
+else:
+    version = '%d.%d' % version_bits[:2]
+
+setup(
+    name = 'djog',
+    version = version,
+    packages = ['djog', 'djog.templatetags',],
+    url = 'http://dev.oebfare.com/projects/djog/',
+    classifiers = ['Programming Language :: Python',]
+)
