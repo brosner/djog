88fa728caf5d87d3c564474208ba27583cc0b8f5
Brian Rosner <brosner@gmail.com>
2007-10-22 17:30:10 +0000

    fixed a problem setup.py needing DJANGO_SETTINGS_MODULE during install.
    git-svn-id: file:///Users/brian/Backups/S3/backups/Wednesday/brian/svn/djog/trunk@44 95ab7de8-677b-4562-a266-b8acc9dd993c

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 
 from setuptools import setup
 
-version_bits = __import__('djog').VERSION
-if version_bits[2] is not None:
-    version = '%d.%d_%s' % version_bits
-else:
-    version = '%d.%d' % version_bits[:2]
-
 setup(
     name = 'djog',
-    version = version,
+    # TODO: dynamically create from djog, but internal dependancies will play
+    # out first.
+    version = '0.1_pre',
     packages = ['djog', 'djog.templatetags',],
     url = 'http://dev.oebfare.com/projects/djog/',
     classifiers = ['Programming Language :: Python',]
