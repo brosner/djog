ac45d295377aac114e2a63f7880d32fabc96305d
Brian Rosner <brosner@gmail.com>
2007-12-13 07:32:19 +0000

    Refs #28. Implemented CommentCountNode.
    git-svn-id: file:///Users/brian/Backups/S3/backups/Wednesday/brian/svn/djog/branches/nf-comments@69 95ab7de8-677b-4562-a266-b8acc9dd993c

--- djog/comments/templatetags/comments.py
+++ djog/comments/templatetags/comments.py
@@ -1,9 +1,14 @@
 
-from django import template
+from django.template import TemplateSyntaxError
+from django.template import Node, Library, Variable
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.contenttypes.models import ContentType
 
-register = template.Library()
+from djog.comments.models import Comment
 
-class CommentFormNode(template.Node):
+register = Library()
+
+class CommentFormNode(Node):
     def __init__(self):
         pass
     
@@ -13,17 +18,60 @@ def do_comment_form(parser, token):
     return CommentFormNode()
 register.tag("comment_form", do_comment_form)
 
-class CommentCountNode(template.Node):
-    def __init__(self):
-        pass
+class CommentCountNode(Node):
+    def __init__(self, package, module, context_var_name, obj_id, var_name):
+        self.package, self.module = package, module
+        if context_var_name is not None:
+            context_var_name = Variable(context_var_name)
+        self.context_var_name, self.obj_id = context_var_name, obj_id
+        self.var_name = var_name
     
     def render(self, context):
+        comment_count = Comment.objects.filter(
+            object_id = self.obj_id,
+            content_type__app_label = self.package,
+            content_type__model = self.module,
+            # site__id = settings.SITE_ID,
+        ).count()
+        context[self.var_name] = comment_count
         return u""
 def do_comment_count(parser, token):
-    return CommentCountNode()
+    bits = token.contents.split()
+    if len(bits) != 6:
+        raise TemplateSyntaxError, "%r tag requires 5 arguments" % bits[0]
+    if bits[1] != "for":
+        raise TemplateSyntaxError,
+            "Second argument in %r tag must be 'for'" % bits[0]
+    try:
+        package, module = bits[2].split(".")
+    except ValueError: # unpack list of wrong size
+        raise TemplateSyntaxError,
+            "Third argument in %r tag must be in the format 'package.module'" % bits[0]
+    try:
+        content_type = ContentType.objects.get(app_label=package, model=module)
+    except ContentType.DoesNotExist:
+        raise TemplateSyntaxError,
+            "%r tag has invalid content-type '%s.%s'" % (bits[0], package, module)
+    var_name, obj_id = None, None
+    if bits[3].isdigit():
+        obj_id = bits[3]
+        try: # ensure the object ID is valid
+            content_type.get_object_for_this_type(pk=obj_id)
+        except ObjectDoesNotExist:
+            raise TemplateSyntaxError,
+                "%r tag refers to %s object with ID %s, which doesn't exist" % (
+                    bits[0],
+                    content_type.name,
+                    obj_id,
+                )
+    else:
+        var_name = bits[3]
+    if bits[4] != "as":
+        raise TemplateSyntaxError, "Fourth argument in %r must be 'as'" % bits[0]
+    return CommentCountNode(package, module, var_name, obj_id, bits[5])
 register.tag("get_comment_count", do_comment_count)
 
-class CommentListNode(template.Node):
+class CommentListNode(Node):
     def __init__(self):
         pass
     
